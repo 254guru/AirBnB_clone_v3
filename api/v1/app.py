ce1803eaea8d22ad10c54c09ff1e36c046be99f4
254guru <otienooluda@gmail.com>
2024-01-10 11:51:10 -0800

    HTTP access control

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,9 +8,13 @@ from models import storage
 from flask import Flask
 from api.v1.views import app_views
 from flask import jsonify
+from flasgger import Swagger
+from flask_cors import CORS
+from flasgger.utils import swag_from
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -25,6 +29,13 @@ def error(error):
     """
     return jsonify({"error": "Not found"}), 404
 
+
+app.config['SWAGGER'] = {
+        'title': 'AirBnB clone Restful API',
+        'uiversion': 3
+        }
+
+Swagger(app)
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
